95be9014e39c08ada510a34f625cc7f403728259
Tomasz Mizak <tomasz.mizak99@gmail.com>
2023-06-11 09:04:29 +0200

    simple validation on author creation

--- src/main/java/com/example/bdia/controller/AuthorController.java
+++ src/main/java/com/example/bdia/controller/AuthorController.java
@@ -4,6 +4,7 @@ import com.example.bdia.model.converter.Author2AuthorDtoConverter;
 import com.example.bdia.model.dto.AuthorDto;
 import com.example.bdia.model.dto.CreateAuthorDto;
 import com.example.bdia.service.AuthorService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ public class AuthorController {
     }
 
     @PostMapping(value = "/create")
-    public ResponseEntity<AuthorDto> createAuthor(@RequestBody CreateAuthorDto createAuthorDto) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody @Valid CreateAuthorDto createAuthorDto) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(author2AuthorDtoConverter.convert(authorService.createAuthor(createAuthorDto)));
     }
--- src/main/java/com/example/bdia/model/dto/CreateAuthorDto.java
+++ src/main/java/com/example/bdia/model/dto/CreateAuthorDto.java
@@ -1,5 +1,8 @@
 package com.example.bdia.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,12 @@ import lombok.Setter;
 @Setter
 @Getter
 public class CreateAuthorDto {
+
+    @Size(min = 4, max = 64)
     private String firstName;
+
     private String secondName;
+
+    @Size(min = 4, max = 64)
     private String familyName;
 }
